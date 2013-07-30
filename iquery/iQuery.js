d90b74132387deacb0bb56998b696cbf852443ce
miamarti <miller.augusto@gmail.com>
2013-07-29 21:11:18 -0300

    Implementando nova estrutura, ainda não funcional...

--- iquery/iQuery.js
+++ iquery/iQuery.js
@@ -4,132 +4,86 @@
  * @returns {Object}
  */
 iQuery = function(){
-	var me = arguments[0]?arguments[0]:'';
-    var core = {};
-    var type = String(me.replace(/^\s+/,'')).substring(0,1);
-    var me = me.replace(/^\s+/,'').replace(type,'');
-    var obj = (type==='#')?document.getElementById(me):document.getElementsByClassName(me);
-
-    Object.defineProperty(core, "html", {
-            value : function(data){
-                if(type === '#'){
-                    obj.innerHTML = data;
-                } else{
-                    for(i=0;i<obj.length;i++){
-                        obj[i].innerHTML = data;
-                    }
-                }
-            },
-            writable : true,
-            configurable : true,
-            enumerable : true
-        }
-    );
-
-    Object.defineProperty(core, "empty", {
-            value : function(){
-                if(type === '#'){
-                    obj.innerHTML = '';
-                } else{
-                    for(i=0;i<obj.length;i++){
-                        obj[i].innerHTML = '';
-                    }
-                }
-            },
-            writable : true,
-            configurable : true,
-            enumerable : true
-        }
-    );
-
-    Object.defineProperty(core, "scroll", {
-            value : function(){
-                 obj.scrollIntoView();
-            },
-            writable : true,
-            configurable : true,
-            enumerable : true
-        }
-    );
-
-    Object.defineProperty(core, "addClass", {
-            value : function(data){
-                if(type === '#'){
-                    obj.className = data;
-                } else{
-                    for(i=0;i<obj.length;i++){
-                        obj[i].className = data;
-                    }
-                }
-            },
-            writable : true,
-            configurable : true,
-            enumerable : true
-        }
-    );
-
-    Object.defineProperty(core, "removeClass", {
-            value : function(data){
-                if(type === '#'){
-                    obj.className = '';
-                } else{
-                    for(i=0;i<obj.length;i++){
-                        obj[i].className = obj[i].className.replace('/\b'+data+'\b/','');
-                    }
-                }
-            },
-            writable : true,
-            configurable : true,
-            enumerable : true
-        }
-    );
-        
-    Object.defineProperty(core, "click", {
-            value : function(data){
-                if(type === '#'){
-                    obj.addEventListener('click', data, false);
-                } else{
-                    for(i=0;i<obj.length;i++){
-                        obj[i].addEventListener('click', data, false);
-                    }
-                }
-            },
-            writable : true,
-            configurable : true,
-            enumerable : true
-        }
-    );
+    var me = arguments[0]?arguments[0]:{};
+    var core;
     
-    Object.defineProperty(core, "load", {
-    		value : function(url, data){
-                var formData = new FormData();
-                for(key in data){
-                	formData.append(key, data[key]);
-                }
-                var client = new XMLHttpRequest();
-                client.open("POST", url, true);
-                client.onload = function(){
-                    if(type === '#'){
-                        obj.innerHTML = this.response;
-                    } else{
-                        for(i=0;i<obj.length;i++){
-                            obj[i].innerHTML = this.response;
-                        }
-                    }
-                };
-                client.send(formData);
-    		}
+    try {
+        core = document.querySelectorAll(me);
+    } catch (e) {
+        core = me;
+    }
+    
+    core.html = function(data){
+        for(var i=0;i<this.length;i++){
+            this[i].innerHTML = data;
+        }
+    };
+   
+    core.empty = function(){
+        for(var i=0;i<this.length;i++){
+            this[i].innerHTML = '';
+        }
+    };
+   
+    core.scroll = function(){
+        for(var i=0;i<this.length;i++){
+            this[i].scrollIntoView();
+        }
+    };
+   
+    core.addClass = function(data){
+        for(var i=0;i<this.length;i++){
+            this[i].className = data;
+        }
+    };
+   
+    core.removeClass = function(){
+        for(var i=0;i<this.length;i++){
+            this[i].className = '';
+        }
+    };
+   
+    core.click = function(data){
+        for(var i=0;i<this.length;i++){
+            this[i].addEventListener('click', data, false);
+        }
+    };
+   
+    core.load = function(url, data){
+        var formData = new FormData();
+        var obj = this;
+        for(key in data){
+            formData.append(key, data[key]);
+        }
+        var client = new XMLHttpRequest();
+        client.open("POST", url, true);
+        client.onload = function(){
+            for(var i=0;i<obj.length;i++){
+                obj[i].innerHTML = this.response;
+            }
+        };
+        client.send(formData);
+    };
+    
+    core.each = function(each){
+        for ( var i = 0; i < this.length; i++) {
+            each(this[i]);
+        }
+    };
+    
+    core.attr = function(attr){
+    	var collection;
+    	if(Object.prototype.toString.call(this) === '[object HTMLSpanElement]'){
+    		collection = this.getAttribute(attr);
+    	} else{
+    		collection = [];
+    		for(var i=0;i<this.length;i++){
+    			collection[collection.length] = this[i].getAttribute(attr);
+    		}    		
     	}
-    );
-        
-    Object.defineProperty(core, "onLine", {
-            value : window.navigator.onLine,
-            writable : true,
-            configurable : true,
-            enumerable : true
-        }
-    );
-    
+        return collection;
+    };
+   
     return core;
 }
 $ = iQuery;
